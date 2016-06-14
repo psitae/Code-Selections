5c6560ab2fa538e91a3507cd90c1fa808db90a8e
Tony <psitae@gmail.com>
2016-06-14 00:32:22 -0400

    Update arctan.m

--- arctan.m
+++ arctan.m
@@ -1,6 +1,7 @@
 function [ angle ] = arctan( a, b )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+
+% extends normal range of arctangent
+
 first = atan(b/a);
 if a<0
     angle = first + pi;
